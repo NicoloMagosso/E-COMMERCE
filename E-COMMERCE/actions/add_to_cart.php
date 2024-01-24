9c97e47dd78ac98b4989141c3d0ecf2b01e3454e
Name <98811739+NicoloMagosso@users.noreply.github.com>
2024-01-24 12:31:12 +0100

    Correzione errore carrello

--- E-COMMERCE/actions/add_to_cart.php
+++ E-COMMERCE/actions/add_to_cart.php
@@ -4,21 +4,23 @@ require '../models/Classes.php';
 
 session_start();
 
-//Ottieni l'oggetto utente corrente dalla sessione
+// Ottieni l'oggetto utente corrente dalla sessione
 $user = $_SESSION['current_user'];
 
-//Definisce un array con 'product_id' e 'quantita' ottenuti dalla richiesta POST
+// Definisce un array con 'product_id' e 'quantita' ottenuti dalla richiesta POST
 $params = ['product_id' => $_POST['product_id'], 'quantita' => $_POST['quantita']];
 
-//Trova il carrello dell'utente corrente
+// Trova il carrello dell'utente corrente
 $cart = Cart::Find($user->getId());
 
-//Verifica se il carrello esiste già o ne crea uno nuovo
+// Verifica se il carrello esiste già o ne crea uno nuovo
 if (!$cart) {
-    $cart->updateProduct($params);
-} else {
-    $cart->AddProductToCart($params);
+    // Se il carrello non esiste, crea un nuovo carrello
+    $cart = Cart::Create($user->getId());
 }
+
+// Aggiorna o aggiungi il prodotto al carrello
+$cart->AddProductToCart($params);
 
 header('Location: ../views/products/index.php');
 exit();
